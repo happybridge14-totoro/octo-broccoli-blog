a2713fff279ece72aa5f54165c235a14575447ba
Yiji Huang <happybridge14@gmail.com>
2020-01-14 18:06:39 -0800

    Implement compare function in compare.js

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -5,6 +5,22 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
-
-  return 0; // this line is wrong
+  let ret = 0;
+  const hashMap = {};
+  for (let char of word) {
+    const lowerCaseChar = char.toLowerCase();
+    if (hashMap[lowerCaseChar]) {
+      hashMap[lowerCaseChar]++;
+    } else {
+      hashMap[lowerCaseChar] = 1;
+    }
+  }
+  for (let char of guess) {
+    const lowerCaseChar = char.toLowerCase();
+    if (hashMap[lowerCaseChar]) {
+      hashMap[lowerCaseChar]--;
+      ret++;
+    }
+  }
+  return ret;
 }
