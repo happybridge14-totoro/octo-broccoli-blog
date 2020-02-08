92dacbc4ca31fee1906749c6409e65f18b371e4c
Yiji Huang <happybridge14@gmail.com>
2020-02-08 10:57:57 -0800

    Use string instead of a html file for index template

--- exams/exam01/src/index.js
+++ exams/exam01/src/index.js
@@ -1,10 +1,4 @@
-// const indexTemplate = require("./index-template");
-// const indexTemplate = require("./index-template");
-const fs = require("fs");
-const path = require("path");
-const encodingOption = 'utf-8';
-const indexTemplatePath = "./index.html";
-const indexHtmlTemplate = fs.readFileSync(path.resolve(__dirname, indexTemplatePath), encodingOption);
+const indexHtmlTemplate = require("./index-template");
 
 const data = require("./model/data");
 const renderInitPage = require("./view/init");
