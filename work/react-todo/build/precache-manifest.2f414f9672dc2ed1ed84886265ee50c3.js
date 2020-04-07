375ad74427c4d994f9858914f690342dce603058
Yiji Huang <happybridge14@gmail.com>
2020-04-06 18:58:26 -0700

    Init projects. Update css file. Add built files

---
+++ work/react-todo/build/precache-manifest.2f414f9672dc2ed1ed84886265ee50c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b450785f667b41504eccc4ff8b9be973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d703b5f7d5fca22525c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "637577138c2b111f6475",
+    "url": "/static/js/2.c9c1886f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9c1886f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d703b5f7d5fca22525c9",
+    "url": "/static/js/main.6e230f15.chunk.js"
+  },
+  {
+    "revision": "07f850ce9391d20fff00",
+    "url": "/static/js/runtime-main.3eb9ba2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
